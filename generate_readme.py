dafcace7b1ea5421d1637e8d24aef0f1cf2daa2c
Bharat Adhikari <51126350+bharatadk@users.noreply.github.com>
2023-03-05 15:28:03 +0545

    bharat/bug_fix replaced forks>>stars

--- generate_readme.py
+++ generate_readme.py
@@ -4,19 +4,27 @@ import requests
 owner = "bharatadk"
 repo = "python_splitter"
 
-# Get the list of forks for the repository using the GitHub API
-response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/forks")
-forks = response.json()
+# Get the list of users who have starred the repository using the GitHub API
+response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/stargazers")
+stargazers = response.json()
 
-# Extract the image URLs for each fork
-images = [fork["owner"]["avatar_url"] for fork in forks]
+# Extract the avatar URLs for each stargazer
+avatars = [stargazer["avatar_url"] for stargazer in stargazers]
 
-# Format the list of forks and their images using Markdown syntax
-content = "## Forks\n\n"
-for i, fork in enumerate(forks):
-    content += f"{i+1}. [{fork['full_name']}]({fork['html_url']})\n"
-    content += f"   ![Image of {fork['owner']['login']}]({images[i]})\n"
+# Get the current content of the README.md file
+with open("README.md", "r") as file:
+    content = file.read()
 
-# Write the formatted content to the README.md file
+# Delete the existing stargazers list from the README.md file
+if "## Starred By\n\n" in content:
+    content = content.split("## Starred By\n\n")[0]
+
+# Append the list of stargazers and their images to the end of the README.md file
+content += "\n## Starred By\n\n"
+for i, stargazer in enumerate(stargazers):
+    content += f"{i+1}. [{stargazer['login']}]({stargazer['html_url']})\n"
+    content += f"   ![Image of {stargazer['login']}]({avatars[i]})\n"
+
+# Write the updated content to the README.md file
 with open("README.md", "w") as file:
     file.write(content)
