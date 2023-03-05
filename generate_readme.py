8b38a641a9071ad9031a363702380a5d24ff9324
Bharat Adhikari <51126350+bharatadk@users.noreply.github.com>
2023-03-05 15:42:27 +0545

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -11,6 +11,16 @@ stargazers = response.json()
 # Extract the avatar URLs for each stargazer
 avatars = [stargazer["avatar_url"] for stargazer in stargazers]
 
+# Generate the rounded avatar images using Avatarify
+avatarify_url = "https://avatarify.ai/avatar"
+avatarify_params = {"url": "", "size": 25, "round": True}
+rounded_avatars = []
+for avatar in avatars:
+    avatarify_params["url"] = avatar
+    response = requests.get(avatarify_url, params=avatarify_params)
+    rounded_avatar = response.content.decode("utf-8")
+    rounded_avatars.append(rounded_avatar)
+
 # Get the current content of the README.md file
 with open("README.md", "r") as file:
     content = file.read()
@@ -22,8 +32,8 @@ if "## Starred By" in content:
 # Append the list of stargazers and their images to the end of the README.md file
 content += "## Starred By\n"
 for i, stargazer in enumerate(stargazers):
-    content += f"[{stargazer['login']}]({stargazer['html_url']})\n"
-    content += f"<img src='{avatars[i]}' width='25' height='25' style='border-radius: 50%;'>\n"
+    content +=f"[![Image of {stargazer['login']}]({rounded_avatars[i]})]({profiles[i]})\n"
+
 
 # Write the updated content to the README.md file
 with open("README.md", "w") as file:
