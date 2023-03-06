8e0cc7131e2de67546c0af918048e8ca9aaafe2f
Bharat Adhikari <51126350+bharatadk@users.noreply.github.com>
2023-03-06 08:23:06 +0545

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -9,9 +9,9 @@ repo = "python_splitter"
 headers = {"Accept": "application/vnd.github.v3+json"}
 response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/stargazers", headers=headers)
 stargazers = response.json()
-
+print(stargazers)
 # Extract the avatar URLs for each stargazer
-avatars = [(stargazer.json)['avatar_url'] for stargazer in stargazers]
+avatars = [(stargazer)['avatar_url'] for stargazer in stargazers]
 avatars = avatars[::-1]
 
 # Get the current content of the README.md file
